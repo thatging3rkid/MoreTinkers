f9f1e617d36b61a3d7beb0f7a7e217e70bed6bb3
WURBO <willurbowicz@gmail.com>
2015-10-02 10:47:14 -0400

    Nickel Block + textures

--- java/com/canadianwurbokid/moretinkers/block/ModBlocks.java
+++ java/com/canadianwurbokid/moretinkers/block/ModBlocks.java
@@ -7,9 +7,11 @@ import net.minecraft.block.material.Material;
 public final class ModBlocks {
 
 	public static Block nickelOre;
+	public static Block nickelBlock;
 
     public static final void init() {
 
     	GameRegistry.registerBlock(nickelOre = new OreBlocks("nickelOre",Material.rock),"nickelOre");
+    	GameRegistry.registerBlock(nickelBlock = new NickelBlock("nickelBlock",Material.iron),"nickelBlock");
     }
 }
