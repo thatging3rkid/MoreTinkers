0e0a8727ef8101e838fd3f4b92caafd595915456
Lorrif22 <nsmith16104@comcast.net>
2015-10-20 10:19:43 -0400

    Titanium Block
    Adds a new block.

--- java/com/canadianwurbokid/moretinkers/block/ModBlocks.java
+++ java/com/canadianwurbokid/moretinkers/block/ModBlocks.java
@@ -8,10 +8,12 @@ public final class ModBlocks {
 
 	public static Block nickelOre;
 	public static Block nickelBlock;
+	public static Block titaniumBlock;
 
     public static final void init() {
 
     	GameRegistry.registerBlock(nickelOre = new OreBlocks("nickelOre",Material.rock),"nickelOre");
     	GameRegistry.registerBlock(nickelBlock = new NickelBlock("nickelBlock",Material.iron),"nickelBlock");
+    	GameRegistry.registerBlock(titaniumBlock = new TitaniumBlock("titaniumBlock",Material.iron),"titaniumBlock");
     }
 }
