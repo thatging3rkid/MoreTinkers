15bd1522bb33072a1694c58ddd7da01f00e66a04
Lorrif22 <nsmith16104@comcast.net>
2015-10-27 18:29:44 -0400

    Titanium Ore and Ingot Textures
    And some other stuff for some reason.

---
+++ java/com/canadianwurbokid/moretinkers/block/ModBlocks (2).java
@@ -0,0 +1,22 @@
+package com.canadianwurbokid.moretinkers.block;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public final class ModBlocks {
+
+	public static Block nickelOre;
+	public static Block nickelBlock;
+	
+	public static Block titaniumOre;
+	public static Block titaniumBlock;
+
+    public static final void init() {
+
+    	GameRegistry.registerBlock(nickelOre = new NickelOre("ore_nickel",Material.rock),"nickelOre");
+    	GameRegistry.registerBlock(nickelBlock = new NickelBlock("nickelBlock",Material.iron),"nickelBlock");
+    	GameRegistry.registerBlock(titaniumOre = new TitaniumOre("ore_titanium",Material.rock), "titaniumOre");
+    	GameRegistry.registerBlock(titaniumBlock = new TitaniumBlock("titaniumBlock",Material.iron), "titaniumBlock");
+    }
+}
