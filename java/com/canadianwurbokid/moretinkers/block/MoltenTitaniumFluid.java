bdc5bb475cd5a0b242489b3ee5c78e64fe8b3371
Lorrif22 <nsmith16104@comcast.net>
2015-11-17 10:28:49 -0500

    Molten Titanium Added... Somewhat
    It's in there... somewhere.

---
+++ java/com/canadianwurbokid/moretinkers/block/MoltenTitaniumFluid.java
@@ -0,0 +1,13 @@
+package com.canadianwurbokid.moretinkers.block;
+
+import net.minecraftforge.fluids.Fluid;
+
+public class MoltenTitaniumFluid extends Fluid{
+
+	public MoltenTitaniumFluid(String fluidName) {
+		super(fluidName);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
