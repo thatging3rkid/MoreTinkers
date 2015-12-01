63ceb9f3810acb76d910d458844d21fd90b23a8e
Lorrif22 <nsmith16104@comcast.net>
2015-12-01 09:50:53 -0500

    Molten Titanium Made Better
    It now burns and flows slower.

--- java/com/canadianwurbokid/moretinkers/block/MoltenTitaniumFluid.java
+++ java/com/canadianwurbokid/moretinkers/block/MoltenTitaniumFluid.java
@@ -1,16 +1,22 @@
 package com.canadianwurbokid.moretinkers.block;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
 
 public class MoltenTitaniumFluid extends Fluid{
 
+	Fluid lava = FluidRegistry.getFluid("lava");
 	public MoltenTitaniumFluid() {
 		super("molten_titanium");
 		super.setUnlocalizedName("molten_titanium");
+		super.setBlock(lava.getBlock());
+		super.setLuminosity(lava.getLuminosity());
+		super.setDensity(lava.getDensity());
+		super.setTemperature(lava.getTemperature());
+		super.setViscosity(lava.getViscosity());
 		//super.setBlock(Blocks.lava);
 		//super.setIcons(commonIcon)
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub 
 	}
 
 	
